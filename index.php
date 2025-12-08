abf82c1221bb7b8f68f8e455c224ebaabdbdff9a
kenzotrindade <hackgz0101@gmail.com>
2025-12-08 16:37:25 +0100

    StructBug

--- index.php
+++ index.php
@@ -6,19 +6,24 @@ $matrice = array_fill(0, 10, array_fill(0, 10, 0));
 
 $tailleMatrice = isset($_POST["tailleMatrice"]) ? (int)$_POST["tailleMatrice"] : 10;
 
-function creerMatrice($taille)
-{
+function creerMatrice($taille){
     return array_fill(0, $taille, array_fill(0, $taille, 0));
 }
 
 $matrice = creerMatrice($tailleMatrice);
 
-function placer($pdo, $grille, $game_id, $player_id)
-{
+function recup($pdo, $game_id, $player_id){
     $sql = "SELECT * FROM ships WHERE game_id = ? AND player_id = ?";
     $query = $pdo->prepare($sql);
     $query->execute([$game_id, $player_id]);
     $bateaux = $query->fetchAll(PDO::FETCH_ASSOC);
+
+    return $bateaux;
+}
+
+function placer($pdo, $grille, $game_id, $player_id){
+
+    $bateaux = recup($pdo,$game_id,$player_id);
 
     foreach ($bateaux as $bateau) {
 
@@ -26,7 +31,6 @@ function placer($pdo, $grille, $game_id, $player_id)
         $y = $bateau['start_y'];
         $taille = $bateau['size'];
         $direction = $bateau['orientation'];
-
 
         for ($i = 0; $i < $taille; $i++) {
             if ($direction == "horizontale") {
@@ -42,16 +46,74 @@ function placer($pdo, $grille, $game_id, $player_id)
     return $grille;
 }
 
-function tirer($grille, $x, $y)
-{
+function tirer($pdo, $game_id, $player_id, $grille, $x, $y){
+
+    $bateaux = recup($pdo,$game_id,$player_id);
+
+    $valeur_case = $grille[$y][$x];
+    $resultat_env = "";
+    $message = "";
+
+
     if ($grille[$y][$x] != 0) {
         $grille[$y][$x] = "X";
+        $resultat_env = "hit";
+        $message = "Touché !";
+
+        foreach ($bateaux as $bateau) {
+            $bx = $bateau['start_x'];
+            $by = $bateau['start_y'];
+            $size = $bateau['size'];
+            $ori = $bateau['orientation'];
+            $touche_trouve = false;
+
+
+            if ($ori == "horizontale") {
+
+                if ($y == $by && $x >= $bx && $x < ($bx + $size)) {
+                    $touche_trouve = true;
+                }
+            } elseif ($ori == "verticale") {
+
+                if ($x == $bx && $y >= $by && $y < ($by + $size)) {
+                    $touche_trouve = true;
+                }
+            }
+
+
+            if ($touche_trouve) {
+                $nouveaux_hits = $bateau['hits'] + 1;
+                
+
+                $upd = $pdo->prepare("UPDATE ships SET hits = ? WHERE id = ?");
+                $upd->execute([$nouveaux_hits, $bateau['id']]);
+
+
+                if ($nouveaux_hits >= $size) {
+                    $message = "COULÉ !!!";
+                }
+                break; 
+            }
+        }
+
     } else {
         $grille[$y][$x] = "O";
+        $resultat_env = "miss";
+        $message = "Plouf !";
     }
+
+    $sql = "INSERT INTO shots (game_id, player_id, x, y, result) VALUES (?, ?, ?, ?, ?)";
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute([$game_id, $player_id, $x, $y, $resultat_env]);
+
     return $grille;
 }
 
 $matrice = creerMatrice(10);
+<<<<<<< HEAD
 $matrice = placer($pdo, $matrice, 1, 1);
 $matrice = tirer($matrice, 2, 3);
+=======
+$matrice = placer($pdo, $matrice, 1, 1); 
+// $matrice = tirer($matrice, 2, 3);
+>>>>>>> 98ec589 (Fonction Tir connecter DB + Touché | Coulé)
