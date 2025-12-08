0f4d9b2a40475bbbffe4db142b7c4b579c2823d5
kenzotrindade <hackgz0101@gmail.com>
2025-12-08 15:00:51 +0100

    GUI_matrice

---
+++ GUI_matrice.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <link rel="stylesheet" href="style.css">
+  <title>Bataille Navale</title>
+</head>
+
+<body>
+  <form method="post">
+    <label>Taille : </label>
+    <input type="number" name="tailleMatrice" placeholder="10" min="5" max="20">
+    <button type="submit">Créer !</button>
+  </form>
+
+  <table>
+    <?php
+    require_once("index.php");
+    for ($i = 0; $i < $tailleMatrice; $i++) {
+      echo "<tr>";
+      for ($j = 0; $j < $tailleMatrice; $j++) {
+        $valeur = $matrice[$i][$j];
+        $classe = $valeur == 0 ? "vide" : "bateau";
+        echo "<td class='$classe'></td>";
+      }
+      echo "</tr>";
+    }
+    ?>
+  </table>
+</body>
+
+</html>
--- index.php
+++ index.php
@@ -4,18 +4,14 @@ require "test_db.php";
 
 $matrice = array_fill(0, 10, array_fill(0, 10, 0));
 
+$tailleMatrice = isset($_POST["tailleMatrice"]) ? (int)$_POST["tailleMatrice"] : 10;
 
+function creerMatrice($taille)
+{
+    return array_fill(0, $taille, array_fill(0, $taille, 0));
+}
 
-
-
-
-
-
-
-
-
-
-
+$matrice = creerMatrice($tailleMatrice);
 
 $game_id_test = 1;
 $player_id_test = 1;
@@ -26,25 +22,26 @@ $query = $pdo->prepare($sql);
 $query->execute([$game_id_test, $player_id_test]);
 $bateaux = $query->fetchAll(PDO::FETCH_ASSOC);
 
-function placer($grille, $taille, $depart_x, $depart_y, $direction){
-    foreach ($bateaux as $bateau){
-        for ($i = 0; $i < $taille; $i++){
-            if ($direction == "horizontale"){
+function placer($grille, $taille, $depart_x, $depart_y, $direction)
+{
+    foreach ($bateaux as $bateau) {
+        for ($i = 0; $i < $taille; $i++) {
+            if ($direction == "horizontale") {
                 $grille[$depart_y][$depart_x + $i] = $taille;
-            } elseif ($direction == "verticale"){
+            } elseif ($direction == "verticale") {
                 $grille[$depart_y + $i][$depart_x] = $taille;
             } else {
                 echo "Veuillez entrer une direction valide !";
                 break;
             }
         }
-
     }
     return $grille;
 }
 
-function tirer($grille, $x, $y){
-    if ($grille[$y][$x] != 0){
+function tirer($grille, $x, $y)
+{
+    if ($grille[$y][$x] != 0) {
         $grille[$y][$x] = "X";
     } else {
         $grille[$y][$x] = "O";
@@ -52,8 +49,6 @@ function tirer($grille, $x, $y){
     return $grille;
 }
 
-$matrice = placer($matrice, 3, 2, 3,"horizontale");
+$matrice = placer($matrice, 3, 2, 3, "horizontale");
 var_dump($matrice);
-$matrice = tirer($matrice,2,3);
-
-?>
+$matrice = tirer($matrice, 2, 3);
